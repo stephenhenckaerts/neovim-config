0b863bb4e54ecc5ebfd8c987b0bde80628f56a21
stephen <stephen.henckaerts@appwise.be>
2023-08-31 20:22:42 +0200

    redone nerdtree

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -1,12 +1,11 @@
--- Map <leader>pv to open NERDTree focused on the current file
 vim.api.nvim_set_keymap('n', '<leader>pv', ':NERDTreeFind<CR>', { noremap = true, silent = true })
 
--- Close NERDTree when a file is opened
+vim.cmd [[
+  autocmd FileType nerdtree setlocal number relativenumber
+]]
+
 vim.g.NERDTreeQuitOnOpen = 1
 
--- Customize NERDTree behavior on `I` and `S`
--- By default, NERDTree uses NERDTree-Map-ActivateNode-* maps.
--- We'll override these to close NERDTree after opening in a split.
 vim.api.nvim_set_keymap('n', '<buffer><silent>I', ':NERDTree-Map-ActivateNode-VSplit<CR>:q<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<buffer><silent>S', ':NERDTree-Map-ActivateNode-Split<CR>:q<CR>', { noremap = true, silent = true })
 
