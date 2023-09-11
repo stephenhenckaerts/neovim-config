014e6f02594149db35cd223f5836f3867e48ec35
stephen <stephen.henckaerts@appwise.be>
2023-09-11 12:47:14 +0200

    Added nerdtree open current file location

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap('n', '<leader>pv', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pv', ':NERDTreeFind<CR>', { noremap = true, silent = true })
 
 vim.cmd [[
   autocmd FileType nerdtree setlocal number relativenumber
@@ -8,3 +8,4 @@ vim.g.NERDTreeQuitOnOpen = 1
 
 vim.api.nvim_set_keymap('n', '<buffer><silent>I', ':NERDTree-Map-ActivateNode-VSplit<CR>:q<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<buffer><silent>S', ':NERDTree-Map-ActivateNode-Split<CR>:q<CR>', { noremap = true, silent = true })
+
